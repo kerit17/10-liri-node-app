507bc722106ca61457ee31fd840db2ff83c83621
Keri Schreckengost <keris@live.com>
2017-07-17 18:36:36 -0600

    updated keys file and rename of liri-index

---
+++ keys.js
@@ -0,0 +1,9 @@
+//Twitter leys
+console.log('this is loaded');
+
+exports.twitterKeys = {
+  consumer_key: 'ks8Zw3x5jR3JuwWf1rRtCIbA1',
+  consumer_secret: 'c2fPm8x3yHYOMcgPf6y3sCzAa7KjWIXEfXemTjUjiX9M3oaxA1',
+  access_token_key: 'c2fPm8x3yHYOMcgPf6y3sCzAa7KjWIXEfXemTjUjiX9M3oaxA1',
+  access_token_secret: 'cOJlFiEcSZA6AnCl57yNeiQPBvzy72dv3F75mfxfGKaSR',
+}
